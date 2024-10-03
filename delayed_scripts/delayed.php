d30b4bdb8f3c3ce3080404a9d9b2eceea3aa2dd6
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-03 16:15:11 +0200

    updated

--- delayed_scripts/delayed.php
+++ delayed_scripts/delayed.php
@@ -1,10 +1,8 @@
 <?php
-    if ($argc) {
+    if ($argc > 1) {
         require_once(__DIR__ . '/../../../../wp-load.php');
-        save_test($argv);
-        // $mpop = new MultipopPlugin();
-        // $func = $mpop->delayed_scripts[$argv[0]];
-        // if ($func) {
-        //     $func(...array_slice($argv, 1));
-        // }
+        $mpop = new MultipopPlugin();
+        if (isset($mpop->delayed_scripts[$argv[1]])) {
+            $mpop->delayed_scripts[$argv[1]](...array_slice($argv, 2));
+        }
     }
