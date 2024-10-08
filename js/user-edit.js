db5840f899638874b73d0dc4cb069e197398124f
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-08 02:59:13 +0200

    updated

--- js/user-edit.js
+++ js/user-edit.js
@@ -28,24 +28,21 @@ window.onload = () => {
     emailEl = document.getElementById('email');
     emailEl.addEventListener('input', () => {
         if (customContainer.style.display !== 'none') {
-            if (emailEl.value == emailOriginal || (_new_email && emailEl.value == primaryEmail)) {
-                console.log(emailEl.value);
-                console.log(primaryEmail);
-                console.log(_new_email && emailEl.value == primaryEmail);
-                sendMailConfirmationButton.style.display = mailConfirmed || (_new_email && emailEl.value == primaryEmail) ? 'none' : 'unset';
-                revokeMailConfirmationButton.style.display = mailConfirmed || (_new_email && emailEl.value == primaryEmail) ? 'unset' : 'none';
+            if (emailEl.value == emailOriginal) {
+                sendMailConfirmationButton.style.display = mailConfirmed ? 'none' : 'unset';
+                revokeMailConfirmationButton.style.display = mailConfirmed ? 'unset' : 'none';
                 if (sendMailConfirmationContEl.style.display != 'none') {
                     sendMailConfirmationEl.checked = false;
                     sendMailConfirmationEl.disabled = true;
                     sendMailConfirmationContEl.style.display = 'none';
-                    confirmedEl.disabled = mailConfirmed || (_new_email && emailEl.value == primaryEmail);
+                    confirmedEl.disabled = mailConfirmed;
                     if (mailConfirmed) {
                         confirmedEl.checked = true;
                     }
-                    sendMailConfirmationButton.disabled = mailConfirmed || (_new_email && emailEl.value == primaryEmail) || confirmedEl.checked;
+                    sendMailConfirmationButton.disabled = mailConfirmed || confirmedEl.checked;
                 } else if (confirmedEl.disabled) {
-                    confirmedEl.disabled = mailConfirmed || (_new_email && emailEl.value == primaryEmail);
-                    confirmedEl.checked = mailConfirmed || (_new_email && emailEl.value == primaryEmail);
+                    confirmedEl.disabled = mailConfirmed;
+                    confirmedEl.checked = mailConfirmed;
                 }
             } else {
                 revokeMailConfirmationButton.style.display = 'none';
