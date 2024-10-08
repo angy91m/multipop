7bdc26db0320bb1b0ffd78386b1204105e315f08
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-08 02:50:34 +0200

    updated

--- js/user-edit.js
+++ js/user-edit.js
@@ -28,8 +28,9 @@ window.onload = () => {
     emailEl = document.getElementById('email');
     emailEl.addEventListener('input', () => {
         if (customContainer.style.display !== 'none') {
-            if (emailEl.value == emailOriginal) {
-                sendMailConfirmationButton.style.display = mailConfirmed ? 'none' : 'unset';
+            if (emailEl.value == emailOriginal || (_new_email && emailEl.value == primaryEmail)) {
+                sendMailConfirmationButton.style.display = mailConfirmed  || (_new_email && emailEl.value == primaryEmail) ? 'none' : 'unset';
+                console.log(sendMailConfirmationButton.style.display);
                 revokeMailConfirmationButton.style.display = mailConfirmed ? 'unset' : 'none';
                 if (sendMailConfirmationContEl.style.display != 'none') {
                     sendMailConfirmationEl.checked = false;
