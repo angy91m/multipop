61a2e3924346a52da9c245fb0f47b4cb5b53b833
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-21 16:15:02 +0200

    updated

--- shortcodes/logged-myaccount.php
+++ shortcodes/logged-myaccount.php
@@ -413,7 +413,7 @@ $parsed_user = $this->myaccount_get_profile($current_user, true, true);
                                         <template v-if="prop.name == mpop_resp_zones">
                                             <span v-html="prop.format(props.row.mpop_resp_zones)"></span>
                                         </template>
-                                        <template v-else>{{prop.format ? prop.format(props.row[prop.name]) : props.row[prop.name]}}</template>
+                                        <template v-else>{{prop.format(props.row[prop.name])}}</template>
                                     </q-td>
                                 </q-tr>
                             </template>
