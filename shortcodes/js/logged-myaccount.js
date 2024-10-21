efb68a28dacb8cbf45ccd7139b78fcd2cd2c38f7
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-21 05:07:15 +0200

    updated

--- shortcodes/js/logged-myaccount.js
+++ shortcodes/js/logged-myaccount.js
@@ -2,10 +2,10 @@ import '/wp-content/plugins/multipop/js/vue3-sfc-loader.js';
 import Fuse from '/wp-content/plugins/multipop/js/fuse.mjs';
 import * as Vue from '/wp-content/plugins/multipop/js/vue.esm-browser.js';
 import IntlTelInput from '/wp-content/plugins/multipop/js/vue-tel-input.js';
-import * as PrimeVue from 'https://unpkg.com/primevue/umd/primevue.min.js';
+import 'https://unpkg.com/primevue/umd/primevue.min.js';
 const { createApp, ref, computed, reactive, onUnmounted, onBeforeMount, defineAsyncComponent, nextTick } = Vue,
 { loadModule } = window['vue3-sfc-loader'];
-console.log(PrimeVue.Config);
+console.log(PrimeVue);
 const vSel = loadModule(`/wp-content/plugins/multipop/js/vue-select.js`, {
     moduleCache: { vue: Vue },
     async getFile(url) {
