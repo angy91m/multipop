558384ae6000c19a58361049afbbed6b9d106b17
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-22 00:10:17 +0200

    updated

--- shortcodes/js/logged-myaccount.js
+++ shortcodes/js/logged-myaccount.js
@@ -65,29 +65,6 @@ userCsvFields = [
     'mpop_org_role',
     'mpop_subscription_notes'
 ],
-menuItems = [{
-    name: 'summary',
-    label: 'Riepilogo'
-}, {
-    name: 'passwordChange',
-    label: 'Cambio password'
-}, {
-    name: 'card',
-    label: 'Tessera'
-}, {
-    separator: true,
-    name: 'users',
-    label: 'Utenti',
-    admin: true
-}, {
-    name: 'subscriptions',
-    label: 'Tessere',
-    admin: true
-}, {
-    name: 'uploadUserCsv',
-    label: 'Carica CSV Utenti',
-    admin: true
-}],
 loggedMyAccountNonce = document.getElementById('mpop-logged-myaccount-nonce').value;
 let searchUsersTimeout, triggerSearchTimeout;
 createApp({
@@ -128,6 +105,29 @@ createApp({
         csvUsers = reactive([]),
         profilePhoneInput = ref('profilePhoneInput'),
         userEditPhoneInput = ref('userEditPhoneInput'),
+        menuItems = ref([{
+            name: 'summary',
+            label: 'Riepilogo'
+        }, {
+            name: 'passwordChange',
+            label: 'Cambio password'
+        }, {
+            name: 'card',
+            label: 'Tessera'
+        }, {
+            separator: true,
+            name: 'users',
+            label: 'Utenti',
+            admin: true
+        }, {
+            name: 'subscriptions',
+            label: 'Tessere',
+            admin: true
+        }, {
+            name: 'uploadUserCsv',
+            label: 'Carica CSV Utenti',
+            admin: true
+        }]),
         userSearchTablePagination = ref({
             sortBy: 'ID',
             descending: false,
