73c537a0b0b05862eae95da9c2a2c690e33cfea2
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-21 11:58:35 +0200

    updated

--- shortcodes/js/logged-myaccount.js
+++ shortcodes/js/logged-myaccount.js
@@ -3,8 +3,8 @@ import Fuse from '/wp-content/plugins/multipop/js/fuse.mjs';
 import * as Vue from '/wp-content/plugins/multipop/js/vue.esm-browser.js';
 import IntlTelInput from '/wp-content/plugins/multipop/js/vue-tel-input.js';
 window.Vue = Vue;
-import '/wp-content/plugins/multipop/js/vue-material/dist/vue-material.min.js';
-console.log(VueMaterial.default);
+import * as VueMaterial from '/wp-content/plugins/multipop/js/vue-material/dist/vue-material.min.js';
+console.log(VueMaterial);
 const { createApp, ref, computed, reactive, onUnmounted, onBeforeMount, defineAsyncComponent, nextTick } = Vue,
 { loadModule } = window['vue3-sfc-loader'],
 loadVueModule = (...modules) => {
