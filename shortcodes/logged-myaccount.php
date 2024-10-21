0619ea2ba692ba7da0c479b7beadbd257a26463d
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-21 03:48:56 +0200

    updated

--- shortcodes/logged-myaccount.php
+++ shortcodes/logged-myaccount.php
@@ -395,7 +395,7 @@ $parsed_user = $this->myaccount_get_profile($current_user, true, true);
                             <button class="mpop-button" @click="changeUserSearchPage(foundUsersPageTotal)" v-if="userSearch.page != foundUsersPageTotal && !pageButtons.includes(foundUsersPageTotal) && userSearch.page +2 <= foundUsersPageTotal" style="width:auto">Fine</button>
                         </div>
                         <br>
-                        <el-table :data="foundUsers" style="width: 100%">
+                        <el-table :data="foundUsers" style="width: 100%" table-layout="fixed">
                             <el-table-column prop="ID" label="ID" />
                             <el-table-column prop="login" label="Login" />
                             <el-table-column prop="email" label="E-mail" />
