db901bb2929d92b416f40505fb1430da375d234a
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-21 15:44:43 +0200

    updated

--- shortcodes/logged-myaccount.php
+++ shortcodes/logged-myaccount.php
@@ -411,9 +411,10 @@ $parsed_user = $this->myaccount_get_profile($current_user, true, true);
                                 <q-tr :props="props">
                                     <q-td v-for="prop in foundUsersColumns" :key="prop.name">
                                         {{ props.row[prop.name] }}
-                                        <!-- <template v-if="prop.name == mpop_resp_zones">
+                                        <template v-if="prop.name == mpop_resp_zones">
                                             <span v-html="prop.format(props.row.mpop_resp_zones)"></span>
-                                        </template> -->
+                                        </template>
+                                        <template v-else>{{prop.format ? prop.format(props.row[prop.name]) : props.row[prop.name]}}</template>
                                     </q-td>
                                 </q-tr>
                             </template>
