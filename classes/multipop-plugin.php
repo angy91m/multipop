18fcd604c3a89704f423b79c31922b1761a2072f
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-23 16:19:38 +0200

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -166,7 +166,7 @@ class MultipopPlugin {
         $name = trim(mb_strtoupper($name, 'UTF-8'));
         $allowed_chars = "a-zàáâäæçčèéêëìíîïòóôöœùúûüšžß";
         if (
-            !preg_match("/^[$allowed_chars][$allowed_chars\', ]*[$allowed_chars\']/")
+            !preg_match("/^[$allowed_chars][$allowed_chars\', ]*[$allowed_chars\']/", $name)
             || mb_strlen(preg_replace("/[^$allowed_chars]/", '', $name),'UTF-8') < 2
             || sanitize_name($name) != $name
         ) {
