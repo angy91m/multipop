a95b9905510993c4b2dfb064e05e9c0da58a760b
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-23 18:10:46 +0200

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -2192,7 +2192,7 @@ class MultipopPlugin {
                 $sub_id = $sub_id['id'];
             }
             $sub_id = intval($sub_id);
-            if ($sub_id) {
+            if (!$sub_id) {
                 return false;
             }
         } else if ($getby == 'card_number') {
@@ -2220,17 +2220,6 @@ class MultipopPlugin {
             ON s.user_id = ln.user_id 
             AND ln.meta_key = 'last_name' "
         ;
-        save_test($wpdb->prepare(
-            "SELECT s.*,
-            users.user_login AS user_login,
-            users.user_email AS user_email,
-            authors.user_login AS author_login,
-            completers.user_login AS completer_login,
-            fn.meta_value AS first_name, 
-            ln.meta_value AS last_name
-            $q_from WHERE s.$getby = $search_format ". ($getby == 'card_number' ? "AND s.year = $year" : '') ." LIMIT 1;",
-            [$sub_id]
-        ));
         $res = $wpdb->get_row(
             $wpdb->prepare(
                 "SELECT s.*,
