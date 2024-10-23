728a99910714af232d5cf18fa9ea1dcdcc73d89f
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-23 16:21:39 +0200

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -168,7 +168,7 @@ class MultipopPlugin {
         if (
             !preg_match("/^[$allowed_chars][$allowed_chars\', ]*[$allowed_chars\']/", $name)
             || mb_strlen(preg_replace("/[^$allowed_chars]/", '', $name),'UTF-8') < 2
-            || $this::sanitize_name($name) != $name
+            || static::sanitize_name($name) != $name
         ) {
             return false;
         }
