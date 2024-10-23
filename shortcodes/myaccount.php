68edca5ed08bc1705a387317f00af57105c725ad
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-23 15:11:12 +0200

    updated

--- shortcodes/myaccount.php
+++ shortcodes/myaccount.php
@@ -5,7 +5,7 @@ if (!$current_user->ID) {
     // NOT LOGGED IN
     if (isset($_REQUEST['mpop_forgot_password'])) {
         require('forgot-password.php');
-    } else if ($_REQUEST['mpop_invite_token']) {
+    } else if (isset($_REQUEST['mpop_invite_token'])) {
         require('invitation-confirm.php');
     } else {
         require('login.php');
