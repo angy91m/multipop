4c5d8189ca637fd34848ea9b1af92562b292819f
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-24 20:45:23 +0200

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -404,7 +404,7 @@ class MultipopPlugin {
                 $this->location_not_found();
             }
             $this->invited_user = $invited_user;
-            $GLOBALS['my_test'] = get_the_ID();
+            $GLOBALS['my_test'] = 'ciao';
         }
     }
 
