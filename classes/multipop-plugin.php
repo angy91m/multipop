51ff155dbb966de12c9717e93e03307ea4b36579
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-25 23:12:57 +0200

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -212,7 +212,7 @@ class MultipopPlugin {
 
     private static function delay_script(string $script, ...$argv) {
         $test = true;
-        return exec('php ' .MULTIPOP_PLUGIN_PATH . 'delayed_scripts/delayed.php ' . $script . ' ' . implode(' ', $argv) . ' >> '.($test? MULTIPOP_PLUGIN_PATH . '/delay_test.log' : '/dev/null').' &');
+        return exec('php ' .MULTIPOP_PLUGIN_PATH . 'delayed_scripts/delayed.php ' . $script . ' ' . implode(' ', $argv) . ' >> '.($test? MULTIPOP_PLUGIN_PATH . '/delay_test.log' : '/dev/null').' 2>&1 &');
     }
 
     // DB PREFIX FOR PLUGIN TABLES
