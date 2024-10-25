002e302f3c89935c85dbe2a29292dc362ac5570f
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-25 13:51:01 +0200

    updated

--- shortcodes/post/invitation-confirm.php
+++ shortcodes/post/invitation-confirm.php
@@ -23,3 +23,63 @@ if (!wp_verify_nonce($post_data['mpop-invite-nonce'], 'mpop-invite')) {
     echo json_encode( $res_data );
     exit;
 }
+switch( $post_data['action'] ) {
+    case 'get_birth_cities':
+        if (!isset($post_data['mpop_birthplace'])) {
+            $res_data['error'] = ['mpop_birthplace'];
+            http_response_code( 400 );
+            echo json_encode( $res_data );
+            exit;
+        }
+        if (!isset($post_data['mpop_birthdate'])) {
+            $res_data['error'] = ['mpop_birthdate'];
+            http_response_code( 400 );
+            echo json_encode( $res_data );
+            exit;
+        }
+        $post_birthdate = '';
+        try {
+            $filtered_comuni = $this->get_birth_cities($post_data['mpop_birthplace'], $post_data['mpop_birthdate']);
+            $res_data['data'] = ['comuni' => $filtered_comuni];
+        } catch (Exception $err) {
+            $res_data['error'] = explode(',',$err->getMessage());
+            http_response_code( 400 );
+            echo json_encode( $res_data );
+            exit;
+        }
+        break;
+    case 'get_billing_cities':
+        if (!isset($post_data['mpop_billing_city'])) {
+            $res_data['error'] = ['mpop_billing_city'];
+            http_response_code( 400 );
+            echo json_encode( $res_data );
+            exit;
+        }
+        try {
+            $filtered_comuni = $this->get_billing_cities($post_data['mpop_billing_city']);
+            $res_data['data'] = ['comuni' => $filtered_comuni];
+        } catch(Exception $e) {
+            $res_data['error'] = explode(',',$err->getMessage());
+            http_response_code( 400 );
+            echo json_encode( $res_data );
+            exit;
+        }
+        break;
+    default:
+        $res_data['error'] = ['action'];
+        $res_data['notices'] = [['type'=>'error', 'msg' => 'Richiesta non valida']];
+        http_response_code( 400 );
+        echo json_encode( $res_data );
+        exit;
+}
+if (!isset($res_data['data'])) {
+    $res_data['error'] = ['action'];
+    $res_data['notices'] = [['type'=>'error', 'msg' => 'Richiesta non valida']];
+    http_response_code( 400 );
+    echo json_encode( $res_data );
+    exit;
+}
+if (isset($res_data['data'])) {
+    echo json_encode( $res_data );
+    exit;
+}
