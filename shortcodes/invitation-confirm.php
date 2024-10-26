2ed68ec2b55bfcc19a82d8cc306c51b07729b9b2
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-26 14:42:08 +0200

    updated

--- shortcodes/invitation-confirm.php
+++ shortcodes/invitation-confirm.php
@@ -26,7 +26,7 @@ $invitation_props = [
     <p v-if="errorFields.has('server')" class="mpop-field-error">Errore sever</p>
     <p v-if="startedFields.has('username') && !isValidUsername() && !errorFields.has('duplicated')" class="mpop-field-error">Il nome utente può contenere solo lettere minuscole, numeri e i simboli . _ -<br>Inoltre non può iniziare e terminare con i simboli . -</p>
     <p v-if="!isValidUsername() && errorFields.has('duplicated')" class="mpop-field-error">Nome utente già registrato</p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <input v-model="user.username" @input="startField('username')" type="text" :class="startedFields.has('username') ? (isValidUsername() ? '' : ' bad-input' ) : ''" id="inv_username" autocomplete="username" placeholder="Nome utente" />
     </p>
     <div v-if="startedFields.has('password') && !isValidPassword()" class="mpop-field-error">
@@ -38,20 +38,20 @@ $invitation_props = [
             <li>Un carattere speciale tra questi: {{ acceptedSymbols }}</li>
         </ul>
     </div>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <input v-model="user.password" @input="startField('password')" type="password" :class="startedFields.has('password') ? (isValidPassword() ? '' : ' bad-input' ) : ''" name="password" id="inv_password" placeholder="Password"/>
     </p>
     <p v-if="startedFields.has('passwordConfirm') && !isValidPasswordConfirm()" class="mpop-field-error">Le password non coincidono</p>
     <p class="mpop-form-row">
         <input v-model="user.password_confirm" @input="startField('passwordConfirm')" type="password" :class="startedFields.has('passwordConfirm') ? (isValidPasswordConfirm() ? '' : ' bad-input' ) : ''" id="inv_password_confirm" placeholder="Conferma password"/>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <input v-model="user.first_name" @input="startField('first_name')" type="text" :class="errorFields.has('first_name') ?  ' bad-input' : ''" id="inv_first_name" placeholder="Nome"/>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <input v-model="user.last_name" @input="startField('last_name')" type="text" :class="errorFields.has('first_name') ?  ' bad-input' : ''" id="inv_last_name" placeholder="Cognome"/>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <label>Data di nascita<br>
         <input type="date"
             :class="errorFields.has('mpop_birthdate') ? 'bad-input' : ''"
@@ -61,7 +61,7 @@ $invitation_props = [
         />
         </label>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <label for="birthplace-select">Comune di nascita</label><br>
         <v-select
             id="birthplace-select"
@@ -99,7 +99,7 @@ $invitation_props = [
             </template>
         </v-select>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <label for="billingCity-select">Comune di residenza</label><br>
         <v-select
             id="billingCity-select"
@@ -148,7 +148,7 @@ $invitation_props = [
             </template>
         </v-select>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <label for="billing-state-select">Provincia di residenza</label><br>
         <select id="billing-state-select" v-model="user.mpop_billing_state" :class="errorFields.has('mpop_billing_state') ? 'bad-input' : ''" disabled>
             <option
@@ -156,7 +156,7 @@ $invitation_props = [
                 :value="user.mpop_billing_city.provincia.sigla">{{user.mpop_billing_city.provincia.sigla}}</option>
         </select>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <label for="billing-zip-select">CAP</label><br>
         <select id="billing-zip-select" v-model="user.mpop_billing_zip" :class="errorFields.has('mpop_billing_zip') ? 'bad-input' : ''" :disabled="!user.mpop_billing_city || user.mpop_billing_city.cap.length == 1">
             <template v-if="user.mpop_billing_city">
@@ -164,11 +164,11 @@ $invitation_props = [
             </template>
         </select>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <label for="billing-address">Indirizzo</label><br>
         <textarea id="billing-address" v-model="user.mpop_billing_address" :class="errorFields.has('mpop_billing_address') ? 'bad-input' : ''" :disabled="!user.mpop_billing_zip"></textarea>
     </p>
-    <p class="mpop-form-row">
+    <p v-if="requireProps" class="mpop-form-row">
         <label for="phone">Telefono</label><br>
         <v-intl-phone
             id="phone"
