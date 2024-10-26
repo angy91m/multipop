ab22b3fe85cc3d69e630e08b1d7bb197105d18a5
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-10-26 14:18:54 +0200

    updated

--- shortcodes/logged-myaccount.php
+++ shortcodes/logged-myaccount.php
@@ -721,7 +721,7 @@ $parsed_user = $this->myaccount_get_profile($current_user, true, true);
                     :pagination="{page:1,rowsPerPage:0}"
                 ></q-table>
                 <div class="q-gutter-sm" style="padding-top: 5px">
-                    <button class="mpop-button" @click="uploadCsvRows" :disabled="saving || !csvUsers.length">Carica righe</button>
+                    <button class="mpop-button" @click="uploadCsvRows" :disabled="saving || !csvUsers.length || csvUsers[0].esito">Carica righe</button>
                     <q-checkbox left-label v-model="csvImportOptions.forceYear" label="Forza anno"></q-checkbox>
                     <q-checkbox left-label v-model="csvImportOptions.forceQuote" label="Forza valore quota"></q-checkbox>
                     <q-checkbox left-label v-model="csvImportOptions.delayedSend" label="Invio ritardato (un invio ogni 5 sec.)"></q-checkbox>
