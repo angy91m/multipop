e9eaf4e94210a2fabb8b7145c51406511089f6e3
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-11-06 00:02:30 +0100

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -904,7 +904,7 @@ class MultipopPlugin {
                         <?php
                         $pdf->writeHTML(ob_get_clean(),true, false, false, false);
                         $pdf->setY(49.5);
-                        $pdf->setX(155);
+                        $pdf->setX(154.7);
                         ob_start(); ?>
                         <span style="font-family: 'helveticamedium'; font-size: 12pt; line-height: 15px;"><?=$bp['provincia']['sigla']?></span>
                         <?php
