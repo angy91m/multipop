6a75fb49fa10693cc9e31b7838f9afe2b5cc64b0
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-11-06 00:08:28 +0100

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -933,8 +933,8 @@ class MultipopPlugin {
         if (isset($options['mpop_birthdate']) && is_string($options['mpop_birthdate']) && $options['mpop_birthdate']) {
             $date_arr = explode('-', $options['mpop_birthdate']);
             $pdf->setPage(1);
-            $pdf->setY(55);
-            $pdf->setX(34);
+            $pdf->setY(54.3);
+            $pdf->setX(35);
             ob_start(); ?>
             <span style="font-family: 'helveticamedium'; font-size: 12pt; line-height: 15px;"><?=$date_arr[2]?></span>
             <?php
