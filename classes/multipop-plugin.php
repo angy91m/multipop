bd62e55147f6cee2f85c557083ed42a7af71d3da
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-11-06 01:16:17 +0100

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -1056,7 +1056,7 @@ class MultipopPlugin {
             $pdf->SetAutoPageBreak(false);
             $total_pages = intval($pdf->getAliasNbPages());
             $pdf->SetFont($pdf->config['font'], 'B', $pdf->config['font_size']);
-            $pdf->Text(30,-($pdf->config['margin_bottom']+$pdf->config['margin_footer']),'CIAO');
+            $pdf->Text(30,$pdf->config['margin_bottom']+$pdf->config['margin_footer'],'CIAO');
             //$pdf->Cell(0, , $options['sub_id'], 0, false, 'C', 0, '', 0, false, 'T', 'M');
             $pdf->SetAutoPageBreak(true, $config['margin_bottom']);
         } 
