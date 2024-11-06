af5fa25313f3a13dea835ac8ec40c1eadc0d4f88
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-11-06 02:10:28 +0100

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -1048,7 +1048,7 @@ class MultipopPlugin {
         }
         if (isset($options['mpop_newsletter_agree'])) {
             $pdf->setPage(1);
-            $pdf->setY(220);
+            $pdf->setY(211);
             if ($options['mpop_newsletter_agree']) {
                 $pdf->setX(11.5);
             } else {
