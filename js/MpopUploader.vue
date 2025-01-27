a6a046f77eca9b6a5bd94a013ecf3a6bc40b96be
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-01-27 22:02:27 +0100

    updated

--- js/MpopUploader.vue
+++ js/MpopUploader.vue
@@ -5,7 +5,7 @@
     </div>
 </template>
 <script setup>
-    import { ref } from 'vue';
+    import { ref, onMounted, onBeforeUnmount } from 'vue';
     const model = defineModel({default: [], type: Array}),
     props = defineProps({
         acceptedMime: {
@@ -13,12 +13,32 @@
         }
     }),
     fileInput = ref(null);
-    
+
+    function readFile(f, b64 = false) {
+        return new Promise( r => {
+            const fr = new FileReader();
+            fr.onload = () => {
+                r(b64 ? fr.result : new Uint8Array(fr.result));
+            }
+            b64 ? fr.readAsDataURL(f) : fr.readAsArrayBuffer(f);
+        });
+    }
     function fileUploadBegin() {
         fileInput.value.click();
     }
-    function handleFileChange() {
-        console.log(fileInput.value);
+    async function handleFileChange() {
+        if (fileInput.value.files.length) {
+            const f = fileInput.value.files[0];
+            let acceptedMime = typeof props.acceptedMime == 'string' ? [props.acceptedMime] : props.acceptedMime;
+            if (
+                (Array.isArray(acceptedMime) && f.type && acceptedMime.includes(f.type))
+                || (!Array.isArray(acceptedMime) && acceptedMime)
+            ) {
+                const fileContent = await readFile(f, true);
+                console.log(fileContent);
+            }
+        }
+        console.log();
     }
     
 </script>
