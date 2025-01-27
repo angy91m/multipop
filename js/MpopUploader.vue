78973503ba6fbe270eb2e300588f3b47f1c78776
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-01-27 22:40:52 +0100

    updated

--- js/MpopUploader.vue
+++ js/MpopUploader.vue
@@ -5,40 +5,52 @@
     </div>
 </template>
 <script setup>
-    import { ref, onMounted, onBeforeUnmount } from 'vue';
+    import { ref, nextTick } from 'vue';
     const model = defineModel({default: [], type: Array}),
     props = defineProps({
         acceptedMime: {
             default: true
+        },
+        binary: {
+            default: false
         }
     }),
+    emit = defineEmits(['change']),
     fileInput = ref(null);
 
-    function readFile(f, b64 = false) {
-        return new Promise( r => {
+    function readFile(f) {
+        return new Promise( (resolve, reject) => {
             const fr = new FileReader();
             fr.onload = () => {
-                r(b64 ? fr.result : new Uint8Array(fr.result));
+                resolve(props.binary ? new Uint8Array(fr.result) : fr.result);
             }
-            b64 ? fr.readAsDataURL(f) : fr.readAsArrayBuffer(f);
+            fr.onerror = e => reject(e);
+            props.binary ? fr.readAsArrayBuffer(f) : fr.readAsDataURL(f);
         });
     }
     function fileUploadBegin() {
         fileInput.value.click();
     }
     async function handleFileChange() {
+        console.log('ciao0');
         if (fileInput.value.files.length) {
-            const f = fileInput.value.files[0];
-            let acceptedMime = typeof props.acceptedMime == 'string' ? [props.acceptedMime] : props.acceptedMime;
-            if (
-                (Array.isArray(acceptedMime) && f.type && acceptedMime.includes(f.type))
-                || (!Array.isArray(acceptedMime) && acceptedMime)
-            ) {
-                const fileContent = await readFile(f, true);
-                console.log(fileContent);
+            console.log('ciao1');
+            try {
+                const f = fileInput.value.files[0];
+                let acceptedMime = typeof props.acceptedMime == 'string' ? [props.acceptedMime] : props.acceptedMime;
+                if (
+                    (Array.isArray(acceptedMime) && acceptedMime.includes(f.type || 'application/octet-stream'))
+                    || (!Array.isArray(acceptedMime) && acceptedMime)
+                ) {
+                    const fileContent = await readFile(f),
+                    fileRead = {meta: f, content: fileContent};
+                    model.value.push(fileRead);
+                    emit('change', fileRead);
+                }
+            } finally {
+                nextTick(() => fileInput.value.value = '');
             }
         }
-        console.log();
     }
     
 </script>
