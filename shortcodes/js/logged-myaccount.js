71aea9d57396fc4fc0055f05e497a8adb3229c69
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-01-31 13:05:19 +0100

    updated

--- shortcodes/js/logged-myaccount.js
+++ shortcodes/js/logged-myaccount.js
@@ -868,20 +868,17 @@ createApp({
                     id: subInView.id
                 });
                 if (res.ok) {
-                    try {
-                        const resData = await res.json();
-                        if (resData.data && Array.isArray(resData.data)) {
-                            subInView.files.length = 0;
-                            for (const k in resData.data) {
-                                subInView.files.push({name: k, content: resData.data[k]});
-                            }
-                        } else {
-                            console.error('Unknown error');
-                        }
-                        generateNotices(resData.notices || []);
-                    } catch (err) {
-                        console.log(err);
+                    const resData = await res.json();
+                    if (resData.data && Array.isArray(resData.data)) {
+                        console.log(resData.data);
+                        // subInView.files.length = 0;
+                        // for (const k in resData.data) {
+                        //     subInView.files.push({name: k, content: resData.data[k]});
+                        // }
+                    } else {
+                        console.error('Unknown error');
                     }
+                    generateNotices(resData.notices || []);
                 } else {
                     try {
                         const {error, notices} = await res.json();
