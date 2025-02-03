81cf418e35962d6c0da64528d2cab9a247b3f34e
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-02-03 04:59:37 +0100

    updated

--- shortcodes/js/logged-myaccount.js
+++ shortcodes/js/logged-myaccount.js
@@ -410,9 +410,8 @@ createApp({
             && ( !userInView.mpop_billing_address || userInEditing.mpop_billing_address.trim() )
             && ( !userInView.mpop_phone || userInEditing.mpop_phone )
         ),
-        validUserAddForm = computed(()=>{
-            console.log(userInAdd);
-            return mailRegex.test(userInAdd.email.trim())
+        validUserAddForm = computed(()=>
+            mailRegex.test(userInAdd.email.trim())
             && userInAdd.first_name.trim()
             && userInAdd.last_name.trim()
             && userInAdd.mpop_birthdate
@@ -422,7 +421,6 @@ createApp({
             && (userInAdd.mpop_billing_country != 'ita' || (userInAdd.mpop_billing_city && userInAdd.mpop_billing_zip))
             && userInAdd.mpop_billing_address.trim()
             && userInAdd.mpop_phone
-        }
         ),
         staticPwdErrors = reactive([]),
         pwdChangeErrors = computed(()=> {
