d4983ae28089acc756b771f80e415c57b0fa0d7b
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-09-30 19:44:04 +0200

    updated

--- js/MpopMap.vue
+++ js/MpopMap.vue
@@ -3,6 +3,8 @@
 </template>
 <script setup>
 import { reactive } from 'vue';
+import L from '/wp-content/plugins/multipop/js/leaflet.js';
+console.log(L);
 const prova = reactive({
     color: 'red'
 });
