e4dea8568f34dcb7fbbb9351013af49f413ac8b5
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-09-30 19:58:25 +0200

    updated

--- js/MpopMap.vue
+++ js/MpopMap.vue
@@ -1,12 +1,24 @@
 <template>
-    <div :style="prova">MAPPA</div>
+    <div :id="elId">MAPPA</div>
 </template>
+<style>
+@import '/wp-content/plugins/multipop/css/leaflet.css';
+</style>
 <script setup>
-import { reactive } from 'vue';
+import { reactive, onBeforeMount } from 'vue';
 import L from '/wp-content/plugins/multipop/js/leaflet.js';
-console.log(L);
-const prova = reactive({
-    color: 'red'
+const makeId = (length = 5) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+},
+elId = ref('mpop-map-' + makeId('5')),
+map = ref(null);
+onBeforeMount(() => {
+   map.value = L.map(elId.value).setView([41.9028, 12.4964], 6);
 });
 </script>
 
