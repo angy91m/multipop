0f94cd01476ffd27249065aa19ae1b1ace3e80cb
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-10-01 21:17:38 +0200

    updated

--- js/MpopSelect.vue
+++ js/MpopSelect.vue
@@ -1,14 +1,20 @@
 <template>
-    <VSelect
-        ref="element"
-    >
-    </VSelect>
+  <VSelect
+    v-model="model"
+    ref="element"
+    v-on="$attrs"
+  >
+    <template v-for="(_, name) in $slots" v-slot:[name]="slotData"><slot :name="name" v-bind="slotData" /></template>
+  </VSelect>
 </template>
 <script setup>
-    import VSelect from '/wp-content/plugins/multipop/js/vue-select.js';
-    import {ref, onMounted} from 'vue';
-    const element = ref('element')
-    onMounted(() => {
-        console.log(element.value);
-    });
+import VSelect from '/wp-content/plugins/multipop/js/vue-select.js';
+import {ref, onMounted, defineModel, useAttrs} from 'vue';
+const element = ref('element'),
+model = defineModel(),
+attrs = useAttrs();
+console.log(attrs);
+onMounted(() => {
+  console.log(element.value);
+});
 </script>
