4e99a02cbdd42598792483cf3d86dbca06d8f672
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-10-02 00:37:59 +0200

    updated

--- classes/events.php
+++ classes/events.php
@@ -143,7 +143,7 @@ class MultipopEventsPlugin {
 
     add_filter('run_wptexturize', function($run_texturize) {
       $p = get_post();
-      if (wp_get_theme()->get_page_templates()[$p->page_template] == 'Eventi') return false;
+      if ((wp_get_theme()->get_page_templates()[$p->page_template] ?? '') == 'Eventi') return false;
       return $run_texturize;
     });
 
