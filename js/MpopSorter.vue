a6e7b65fb29d6c6b899165e841d528a75c9b6f23
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-10-07 21:34:01 +0200

    updated

--- js/MpopSorter.vue
+++ js/MpopSorter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
         <div class="column col">
-            <q-select v-model="activeKey" :options="props.options" v-bind="$attrs" />
+            <q-select v-model="activeOption" :options="props.options" v-bind="$attrs" />
         </div>
         <div class="column col">
             <q-btn :icon="activeOrder ? 'arrow_drop_up' : 'arrow_drop_down'" @click="invertOrder"/>
@@ -23,15 +23,14 @@ modelValue = defineModel({
     type: Object,
     required: true
 }),
-activeKey = computed({
-    get: () => Object.keys(modelValue.value)[0],
+activeOption = computed({
+    get: () => props.options.find(o => o.value == Object.keys(modelValue.value)[0]),
     set: v => {
         console.log(v);
     }
 }),
 activeOrder = computed(()=>Object.values(modelValue.value)[0]);
 function invertOrder() {
-    console.log(modelValue);
     modelValue.value[Object.keys(modelValue.value)[0]] = !Object.values(modelValue.value)[0];
 }
 </script>
