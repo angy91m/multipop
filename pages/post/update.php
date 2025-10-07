1dd94c104a27019b19d4a94c50616fd84f1a277a
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-10-08 01:00:17 +0200

    updated

--- pages/post/update.php
+++ pages/post/update.php
@@ -3,5 +3,12 @@ defined( 'ABSPATH' ) || exit;
 if ( !wp_verify_nonce( $_REQUEST['mpop-admin-update-nonce'], 'mpop-admin-update' ) ) {
   $this->add_admin_notice("Invalid request");
 } elseif (isset($_POST["submit"])) {
-  save_test($_FILES["update"]["tmp_name"]);
+  $zip = new ZipArchive;
+  $res = $zip->open($_FILES["update"]["tmp_name"]);
+  if ($res === TRUE) {
+    $zip->extractTo('/myzips/extract_path/');
+    $zip->close();
+    echo 'woot!';
+  }
 }
+@unlink($_FILES["update"]["tmp_name"]);
