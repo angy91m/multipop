2b2d0ac3018aec569a0fab3771f0aa8b2fa320a2
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-10-07 16:34:39 +0200

    updated

--- shortcodes/events.php
+++ shortcodes/events.php
@@ -82,7 +82,7 @@ $found_events = MultipopEventsPlugin::search_events($_GET);
           <div class="row justify-center" v-for="(event, k) in events" :key="k">
             <q-card flat bordered style="min-width: 412px; margin-bottom: 10px;">
               <q-card-section horizontal>
-                <q-card-section class="q-pt-xs">
+                <q-card-section class="q-pt-xs" style="padding: 0 10px">
                   <div
                     class="text-overline"
                     v-html="showEventDate(event)"
