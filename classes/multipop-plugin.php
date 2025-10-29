c05783e1623aea39f190450b9cc09579e04e12b4
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-10-29 23:34:02 +0100

    updated

--- classes/multipop-plugin.php
+++ classes/multipop-plugin.php
@@ -760,7 +760,7 @@ class MultipopPlugin {
     public function admin_init() {
         $current_user = wp_get_current_user();
 
-        if (count($current_user->roles) == 1 && in_array($current_user->roles[0], ['multipopolano','multipopolare_resp','multipopolare_friend']) && !$user->has_cap(MultipopEventsPlugin::CAPS[0])) {
+        if (count($current_user->roles) == 1 && in_array($current_user->roles[0], ['multipopolano','multipopolare_resp','multipopolare_friend']) && !$current_user->has_cap(MultipopEventsPlugin::CAPS[0])) {
             wp_redirect(site_url('/'), 303);
             exit();
         }
