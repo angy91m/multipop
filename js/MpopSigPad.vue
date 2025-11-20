9bcecf41601f0f8bbe913db0f8cce9c42f89af72
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-11-20 20:11:56 +0100

    updated

--- js/MpopSigPad.vue
+++ js/MpopSigPad.vue
@@ -16,6 +16,8 @@ function resizeCanvas() {
   ratio =  Math.max(devicePixelRatio || 1, 1);
   canvas.width = canvas.offsetWidth * ratio;
   canvas.height = canvas.offsetHeight * ratio;
+  console.log(canvas.offsetWidth * ratio);
+  console.log(canvas.offsetHeight * ratio);
   canvas.getContext("2d").scale(ratio, ratio);
   sigPad.clear();
 }
