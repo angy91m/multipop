742f726a0f62d9cde63ff85910797d5f49339b4a
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-11-20 19:02:51 +0100

    updated

--- js/MpopSigPad.vue
+++ js/MpopSigPad.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    <canvas ref="canvas">
-
-    </canvas>
+    <canvas ref="canvas"></canvas>
   </div>
 </template>
 <script setup>
-import {useTemplateRef, onMounted} from 'vue';
+import {useTemplateRef, onMounted, beforeUnmount, defineExpose} from 'vue';
 import SignaturePad from 'signature_pad';
-const canvas = useTemplateRef('canvas');
+const canvasRef = useTemplateRef('canvas');
+let sigPad;
+function resizeCanvas() {
+  const canvas = canvasRef.value,
+  ratio =  Math.max(devicePixelRatio || 1, 1);
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
+  sigPad.clear();
+}
+defineExpose({
+  canvas: canvasRef,
+  sigPad
+});
 onMounted(()=>{
-  console.log(SignaturePad);
-  console.log(canvas);
+  sigPad = new SignaturePad(canvasRef.value);
+  addEventListener('resize', resizeCanvas);
+});
+beforeUnmount(()=>{
+  removeEventListener('resize', resizeCanvas);
 });
 </script>
