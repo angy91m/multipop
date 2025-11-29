5fdda50a380778fc1087c8d02727fd2ff8f39629
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-11-29 21:02:17 +0100

    updated

--- pages/post/update.php
+++ pages/post/update.php
@@ -11,6 +11,8 @@ if ( !wp_verify_nonce( $_REQUEST['mpop-admin-update-nonce'], 'mpop-admin-update'
     if (file_exists(MULTIPOP_PLUGIN_PATH . '/._update/.update_list')) {
       $lines = preg_split("/(\r\n|\n|\r)/",file_get_contents(MULTIPOP_PLUGIN_PATH . '/._update/.update_list'));
       foreach($lines as $line) {
+        $line = trim($line);
+        if (!$line) continue;
         if (file_exists(MULTIPOP_PLUGIN_PATH . '/._update/' . $line )) {
           rename(MULTIPOP_PLUGIN_PATH . '/._update/' . $line, MULTIPOP_PLUGIN_PATH . '/' . $line);
         }
